90d75460dd09a41b2467db7dbaac1e2a7da88d1b
andreas.kaiser <andreas.kaiser@stud.h-da.de>
2019-05-27 22:28:39 +1000

    LostReportViewController: add Photo button removed and instead in collection view put

--- iLost/ViewController/LostReportViewController.swift
+++ iLost/ViewController/LostReportViewController.swift
@@ -16,6 +16,9 @@ class LostReportViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var dateTextField: UITextField!
     @IBOutlet weak var categoryTextField: UITextField!
+    @IBOutlet weak var imagesCollectionView: UICollectionView!
+
+
     var images = [UIImage]()
     var imagePicker: ImagePickerHelper!
     var locationsCoordinates = [CLLocationCoordinate2D]()
@@ -23,10 +26,135 @@ class LostReportViewController: UIViewController {
     var firebase = FirebaseHelper()
     var map:MapHelper?
 
+    //MARK: - Unwind
+    @IBAction func unwindToLostReportViewController(segue: UIStoryboardSegue) {
+        dateTextField.text = lostItem.dateLost
+    }
+
+
+
+    @IBAction func UnwindToLostViewController(segue: UIStoryboardSegue) {
+        print("unwind")
+    }
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.imagePicker = ImagePickerHelper(presentationController: self, delegate: self)
+        imagesCollectionView.delegate = self
+        imagesCollectionView.dataSource = self
+        map = MapHelper(delegate: self, mapView: mapView)
+        setTapGetureOnDateTextField()
+        addPlusImageToImages()
+    }
+
+
+    fileprivate func addPlusImageToImages() {
+        if let image = UIImage(named: "plus"){
+            images.append(image)
+        }
+    }
+}
+
+
+extension LostReportViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1     //return number of sections in collection view
+    }
+
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        if indexPath.row == images.count - 1 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AddCollectionViewCell", for: indexPath) as! AddImageCollectionViewCell
+            let image = images[indexPath.row]
+            cell.imageView.image = image
+            cell.delegate = self
+            return cell
+        }else{
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! LostItemCollectionViewCell
+            let image = images[indexPath.row]
+            cell.imageView.image = image
+            cell.indexPath = indexPath
+            cell.delegate = self
+            return cell
+        }
+    }
+
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if !(indexPath.row == images.count - 1) {
+            performSegue(withIdentifier: "ImageSegue", sender: nil)
+        }
+    }
+
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ImageSegue" {
+            if let viewController = segue.destination as? ImageViewController {
+                var imagesTemp = images
+                imagesTemp.removeLast(1)
+                viewController.images = imagesTemp
+                viewController.currentImage = imagesCollectionView.indexPathsForSelectedItems!.first!.row
+            }
+        }
+    }
+}
+
+
+extension LostReportViewController: DeleteImageCollectionViewDelegate {
+    func deleteImage(indexPath: IndexPath) {
+        images.remove(at: indexPath.row)
+        imagesCollectionView.reloadData()
+    }
+}
+
+
+extension LostReportViewController: AddImageCollectionViewDelegate {
+    func addImage() {
+          self.imagePicker.present(view: self.view)
+    }
+}
+
+
+extension LostReportViewController: ImagePickerDelegate {
+    func selectedImage(image: UIImage?) {
+        guard let image = image else { return }
+        // self.images.append(image)
+        self.images.insert(image, at: 0)
+        imagesCollectionView.reloadData()
+    }
+}
+
+//MARK: - Gestures
+extension LostReportViewController {
+    @IBAction func addPinGesture(_ sender: UILongPressGestureRecognizer) {
+        if sender.state == .began {
+            map!.setAnnotationWithLongPress(sender: sender)
+        }
+    }
+
+    fileprivate func setTapGetureOnDateTextField() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(performSegueSetDate))
+        dateTextField.isUserInteractionEnabled = true
+        dateTextField.addGestureRecognizer(tapGesture)
+    }
+
+
+    @objc func performSegueSetDate(){
+        performSegue(withIdentifier: "SetDateSegue", sender: nil)
+    }
+
 
     @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
         if let category = categoryTextField.text {
-             lostItem.category = category
+            lostItem.category = category
         }
         if let date = dateTextField.text {
             lostItem.dateLost = date
@@ -41,105 +169,15 @@ class LostReportViewController: UIViewController {
             firebase.saveImage(data: data, item: "Item1", fileName: "Image1")
         }
     }
-
-    @IBAction func unwindToLostReportViewController(segue: UIStoryboardSegue) {
-
-        dateTextField.text = lostItem.dateLost
-
-    }
-
-    @IBOutlet weak var imagesCollectionView: UICollectionView!
-    @IBAction func addImagePressed(_ sender: Any) {
-         self.imagePicker.present(view: sender as! UIView)
-    }
-
-    @IBAction func UnwindToLostViewController(segue: UIStoryboardSegue) {
-        print("unwind")
-    }
-
-    fileprivate func setTapGetureOnDateTextField() {
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(performSegueSetDate))
-        dateTextField.isUserInteractionEnabled = true
-        dateTextField.addGestureRecognizer(tapGesture)
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.imagePicker = ImagePickerHelper(presentationController: self, delegate: self)
-        imagesCollectionView.delegate = self
-        imagesCollectionView.dataSource = self
-        map = MapHelper(delegate: self, mapView: mapView)
-
-        setTapGetureOnDateTextField()
-
-    }
-    
-    @objc func performSegueSetDate(){
-        performSegue(withIdentifier: "SetDateSegue", sender: nil)
-    }
-
 }
 
-extension LostReportViewController: ImagePickerDelegate {
-    func selectedImage(image: UIImage?) {
-        guard let image = image else { return }
-         self.images.append(image)
-         imagesCollectionView.reloadData()
-    }
-}
-
-extension LostReportViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        return 1     //return number of sections in collection view
-    }
-
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return images.count
-    }
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! LostItemCollectionViewCell
-        let image = images[indexPath.row]
-        cell.imageView.image = image
-        cell.indexPath = indexPath
-        cell.delegate = self
-        return cell
-    }
-
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "ImageSegue", sender: nil)
-    }
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "ImageSegue" {
-            if let viewController = segue.destination as? ImageViewController {
-                 viewController.images = images
-                 viewController.currentImage = imagesCollectionView.indexPathsForSelectedItems!.first!.row
-            }
-        }
-    }
-}
-
-extension LostReportViewController: DeleteImageCollectionViewDelegate {
-    func deleteImage(indexPath: IndexPath) {
-        images.remove(at: indexPath.row)
-        imagesCollectionView.reloadData()
-    }
-}
-
-extension LostReportViewController {
-    @IBAction func addPinGesture(_ sender: UILongPressGestureRecognizer) {
-        if sender.state == .began {
-            map!.setAnnotationWithLongPress(sender: sender)
-        }
-    }
-}
-
+// MARK: - Map
 extension LostReportViewController: MapViewDelegate {
     func annotationRemove(lostLocationsCoordinates: [CLLocationCoordinate2D]) {
         locationsCoordinates = lostLocationsCoordinates
         print("delegate annot remvove")
     }
+
 
     func annotaionSet(lostLocationsCoordinates: [CLLocationCoordinate2D]) {
       locationsCoordinates = lostLocationsCoordinates
