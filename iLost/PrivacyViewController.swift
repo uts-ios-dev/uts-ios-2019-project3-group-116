7acae7b8c023fa9714ff88397b995a93a5dca667
Vishnu Akhil <vishnu.akhil10@gmail.com>
2019-06-02 11:27:56 +1000

    I added two screens for privacy and contact

---
+++ iLost/PrivacyViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PrivacyViewController.swift
+//  iLost
+//
+//  Created by Vishnu akhil Upparapalle on 2/6/19.
+//  Copyright © 2019 ak. All rights reserved.
+//
+
+import UIKit
+
+class PrivacyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
